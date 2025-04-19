8da3187ff10f8c22bf3e8572b0db3c0ad2b18b31
ayang <473033518@qq.com>
2025-04-19 22:52:35 +0800

    refactor: improve the example code

--- examples/tauri-app/src/App.tsx
+++ examples/tauri-app/src/App.tsx
@@ -1,28 +1,105 @@
-import { useEffect } from "react";
+import { convertFileSrc } from "@tauri-apps/api/core";
+import { Image, Table, TableProps } from "antd";
+import { filesize } from "filesize";
+import { useEffect, useState } from "react";
 import {
   startListening,
   onClipboardChange,
-  writeRTF,
+  ReadClipboardItem,
 } from "tauri-plugin-clipboard-x-api";
 
 const App = () => {
+  const [data, setData] = useState<ReadClipboardItem[]>();
+
   useEffect(() => {
     startListening();
 
     onClipboardChange((result) => {
-      console.log("result", result);
+      setData(Object.values(result));
     });
-
-    setTimeout(() => {
-      try {
-        writeRTF("Hello, world!", "{\\rtf1 Hello, world!}");
-      } catch (error) {
-        console.log("error", error);
-      }
-    }, 3000);
   }, []);
 
-  return <div>App</div>;
+  const columns: TableProps<ReadClipboardItem>["columns"] = [
+    {
+      title: "类型",
+      dataIndex: "type",
+      key: "type",
+      render: (type) => {
+        switch (type) {
+          case "text":
+            return "纯文本";
+          case "rtf":
+            return "富文本";
+          case "html":
+            return "HTML";
+          case "image":
+            return "图像";
+          case "files":
+            return "文件";
+        }
+      },
+    },
+    {
+      title: "内容",
+      dataIndex: "value",
+      key: "value",
+      render: (value, item) => {
+        const { type } = item;
+
+        switch (type) {
+          case "files":
+            return `复制了${value.length}个文件`;
+          case "image":
+            return <Image width={100} src={convertFileSrc(value)} />;
+          case "html":
+            return <div dangerouslySetInnerHTML={{ __html: value }} />;
+          default:
+            return value;
+        }
+      },
+    },
+    {
+      title: "大小",
+      dataIndex: "count",
+      key: "count",
+      render: (count, item) => {
+        const { type } = item;
+
+        switch (type) {
+          case "image":
+          case "files":
+            return filesize(count, { standard: "jedec" });
+          default:
+            return `${count}个字符`;
+        }
+      },
+    },
+    {
+      title: "图像宽度",
+      dataIndex: "width",
+      key: "count",
+      render: (width) => {
+        return width ?? "-";
+      },
+    },
+    {
+      title: "图像高度",
+      dataIndex: "height",
+      key: "count",
+      render: (height) => {
+        return height ?? "-";
+      },
+    },
+  ];
+
+  return (
+    <Table
+      rowKey="type"
+      columns={columns}
+      dataSource={data}
+      pagination={false}
+    />
+  );
 };
 
 export default App;
